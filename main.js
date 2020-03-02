674747241ac4e71e5ac88a379bff577456dfae36
Nazar Burban <BurbaNazar98@gmail.com>
2020-03-02 16:41:36 +0200

    milestone_4 from milestone_3

--- main.js
+++ main.js
@@ -2,42 +2,50 @@ let array = [
     {
         id: 1,
         value: 'Value 1',
-        deleted: false
+        deleted: false,
+        is_visible: true
     },
     {
         id: 2,
         value: 'Value 2',
-        deleted: true
+        deleted: true,
+        is_visible: true
     },
     {
         id: 3,
         value: 'Value 3',
-        deleted: false
+        deleted: false,
+        is_visible: false
     },
     {
         id: 4,
         value: 'Value 4',
-        deleted: true
+        deleted: true,
+        is_visible: true
     },
     {
         id: 5,
         value: 'Value 5',
-        deleted: false
+        deleted: false,
+        is_visible: true
     }
 ];
 
 // milestone_3
+// milestone_4
 
 function init() {
 
     let milestone_3 = [];
+    let milestone_4 = [];
 
     array.map((item) => {
-        if (!item.deleted) {
+        if (!item.deleted && item.is_visible) {
             milestone_3.push(item);
+            milestone_4.push(item);
         }
     });
-    
+
 }
 
 init();
