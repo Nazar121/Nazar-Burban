23759c2f5318b30477c6d703cda93a45560f1be4
Nazar Burban <BurbaNazar98@gmail.com>
2020-03-02 15:26:14 +0200

    milestone_3 from milestone_1_2

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 let array = [
     {
         id: 1,
-        value: 'Value 1'
+        value: 'Value 1',
+        deleted: false
     },
     {
         id: 2,
-        value: 'Value 2'
+        value: 'Value 2',
+        deleted: true
     },
     {
         id: 3,
-        value: 'Value 3'
+        value: 'Value 3',
+        deleted: false
     },
     {
         id: 4,
-        value: 'Value 4'
+        value: 'Value 4',
+        deleted: true
     },
     {
         id: 5,
-        value: 'Value 5'
+        value: 'Value 5',
+        deleted: false
     }
 ];
 
 // milestone_1_2
+// milestone_3
 
 function init() {
 
     let milestone_1_2 = [];
+    let milestone_3 = [];
 
     array.map((item) => {
-        milestone_1_2.push(item);
+        if (!item.deleted) {
+            milestone_1_2.push(item);
+            milestone_3.push(item);
+        }
     });
     
 }
