eafef9e6b5faa06d361629c0681d24770c334778
Nazar Burban <BurbaNazar98@gmail.com>
2020-03-02 15:07:05 +0200

    milestone_1_2 from master

---
+++ main.js
@@ -0,0 +1,36 @@
+let array = [
+    {
+        id: 1,
+        value: 'Value 1'
+    },
+    {
+        id: 2,
+        value: 'Value 2'
+    },
+    {
+        id: 3,
+        value: 'Value 3'
+    },
+    {
+        id: 4,
+        value: 'Value 4'
+    },
+    {
+        id: 5,
+        value: 'Value 5'
+    }
+];
+
+// milestone_1_2
+
+function init() {
+
+    let milestone_1_2 = [];
+
+    array.map((item) => {
+        milestone_1_2.push(item);
+    });
+    
+}
+
+init();
